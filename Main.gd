a8a838ca95fccc2a835ae202d9a2a16fa5f15b8f
Domogo <domsatrapa@gmail.com>
2020-01-08 16:04:22 +0100

    bonus multiplier

--- Main.gd
+++ Main.gd
@@ -8,6 +8,8 @@ var score = 0 setget set_score
 var highscore = 0
 var new_highscore = false
 var level = 0
+var num_circles = 0
+var bonus = 0 setget set_bonus
 
 func _ready():
 	randomize()
@@ -18,7 +20,9 @@ func new_game():
 	# set_score == self.score
 	# set_score(0)
 	self.score = 0
+	self.bonus = 0
 	level = 0
+	num_circles = 0
 	new_highscore = false
 	$HUD.update_score(score)
 	$Camera2D.position = $StartPosition.position
@@ -40,23 +44,26 @@ func spawn_circle(_position=null):
 		var y = rand_range(-500, -400)
 		_position = player.target.position + Vector2(x, y)
 	add_child(c)
+	c.connect("full_orbit", self, "set_bonus", [1])
 	c.init(_position, level)
 
 func _on_Jumper_captured(object):
 	$Camera2D.position = object.position
 	object.capture(player)
 	call_deferred("spawn_circle")
-	self.score += 1
+	self.score += 1 * bonus
+	self.bonus += 1
+	num_circles += 1
+	if num_circles > 0 and num_circles % settings.circles_per_level == 0:
+		level += 1
+		$HUD.show_message("Level %s" % str(level))
 
 func set_score(value):
 	score = value
-	if score > highscroe and !new_highscore:
+	if score > highscore and !new_highscore:
 		$HUD.show_message("New records!")
-		new_highscore = True
+		new_highscore = true
 	$HUD.update_score(score)
-	if score > 0 and score % settings.circles_per_level == 0:
-		level += 1
-		$HUD.show_message("Level %s" % str(level))
 
 func _on_Jumper_died():
 	if score > highscore:
@@ -81,3 +88,6 @@ func save_score():
 	f.store_var(highscore)
 	f.close()
 
+func set_bonus(value):
+	bonus = value
+	$HUD.update_bonus(bonus)
--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,14 +1,25 @@
 extends CanvasLayer
 
+var score = 0
+
 func show_message(text):
 	$Message.text = text
-	$AnimationPlayer.play("show_message")
+	$MessageAnimation.play("show_message")
 
 func hide():
 	$ScoreBox.hide()
+	$BonusBox.hide()
+	$Message.hide()
 
 func show():
 	$ScoreBox.show()
+	$BonusBox.show()
+	$Message.show()
 
 func update_score(value):
 	$ScoreBox/HBoxContainer/Score.text = str(value)
+
+func update_bonus(value):
+	$BonusBox/Bonus.text = str(value) + "x"
+	if value > 1:
+		$BonusAnimation.play("bonus")
